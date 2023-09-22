cd176eb33ea1556bc302192478895ce5ce66c9df
seongjin009 <iseongjin009@gmail.com>
2023-09-22 10:19:55 +0900

    사진추가

--- main.js
+++ main.js
@@ -8,15 +8,28 @@ rejected : 작업의 요청이 거절된 상태
 작업시간이 오래걸리는 업무를 수행할 때 프로미스로 반환하면
 해당 작업완료이후 동기적으로 다음 작업을 선형화해서 수행 (ess)
 */
+
+const section = document.querySelector('section');
+
 fetch('DB/department.json')
-	.then((data) => {
-		//첫번째 then구문에 받아진 promise를 다시 바로 리턴하면
-		//두번째 then구문에서 해당 데이터를 동기적으로 활용가능
-		return data.json();
-	})
+	.then((data) => data.json())
 	.then((json) => {
-		console.log(json);
+		console.log(json.members);
+		let tags = '';
+		json.members.map((data) => {
+			tags += `
+        <article>
+          <div class='pic'>
+            <img src='img/${data.pic}'>
+          </div>
+          <h2>${data.name}</h2>
+          <p>${data.position}</p>
+        </article>
+      `;
+		});
+		section.innerHTML = tags;
 	})
+
 	.catch((err) => {
 		console.log(err);
 	});
